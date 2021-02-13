cdbb350db6a564d15e695334799ba9f88de9f82a
Cesar Amaya <cesar.amaya@gmail.com>
2021-02-13 12:27:29 -0600

    Correct generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,9 +4,9 @@
 
 echo "FROM nginxdemos/hello:plain-text"
 
-#echo "RUN apk update && apk add openssl && mkdir /etc/nginx/certs"
+echo "RUN apk update && apk add openssl && mkdir /etc/nginx/certs"
 
-#echo "ENV ENVIRONMENT=$(awk -F'_' '{print tolower($2)}' <<< $ENVIRONMENT)"
+echo "ENV ENVIRONMENT=$(awk -F'_' '{print tolower($2)}' <<< $ENVIRONMENT)"
 
 #echo "CMD [\"/bin/sh\", \"-c\", \"envsubst '${ENVIRONMENT}' < /nginx.conf.template > /etc/nginx/conf.d/nginx.conf && exec nginx -g 'daemon off;'\"]"
 
@@ -88,53 +88,4 @@ RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSI
     tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
     rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 EOF
-fi
-
-# install bats for testing
-echo "RUN git clone https://github.com/sstephenson/bats.git \
-  && cd bats \
-  && ./install.sh /usr/local \
-  && cd .. \
-  && rm -rf bats"
-
-# install dependencies for tap-to-junit
-echo "RUN perl -MCPAN -e 'install TAP::Parser'"
-echo "RUN perl -MCPAN -e 'install XML::Generator'"
-
-# install lsb-release, etc., for testing linux distro
-echo "RUN apt-get update && apt-get -y install lsb-release unzip"
-
-if [ $BROWSERS = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports xvfb phantomjs \\
-; else \\
-		apt-get update && apt-get -y install xvfb phantomjs \\
-; fi
-EOF
-echo "ENV DISPLAY :99"
-
-echo "# install firefox
-RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.deb https://s3.amazonaws.com/circle-downloads/firefox-mozilla-build_47.0.1-0ubuntu1_amd64.deb \
-  && echo 'ef016febe5ec4eaf7d455a34579834bcde7703cb0818c80044f4d148df8473bb  /tmp/firefox.deb' | sha256sum -c \
-  && dpkg -i /tmp/firefox.deb || apt-get -f install  \
-  && apt-get install -y libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3 \
-  && rm -rf /tmp/firefox.deb"
-
-echo "# install chrome
-RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-  && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || apt-get -fy install)  \
-  && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
-  && sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox --no-sandbox|g' \
-       \"/opt/google/chrome/google-chrome\""
-
-echo "# install chromedriver
-RUN apt-get -y install libgconf-2-4 \
-  && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip \"http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip\" \
-  && cd /tmp \
-  && unzip chromedriver_linux64.zip \
-  && rm -rf chromedriver_linux64.zip \
-  && mv chromedriver /usr/local/bin/chromedriver \
-  && chmod +x /usr/local/bin/chromedriver"
 fi
