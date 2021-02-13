0f5f9cc8b88c5e12b1cb733d6fd1ab308cf5c1c8
Cesar Amaya <cesar.amaya@gmail.com>
2021-02-13 13:23:58 -0600

    add privkey

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,6 +6,12 @@ echo "FROM nginxdemos/hello:plain-text"
 
 echo "RUN apk update && apk add openssl && mkdir /etc/nginx/certs"
 
+echo "ADD cert/fullchain.pem /etc/nginx/certs/"
+echo "ADD $PRIVKEY > /etc/nginx/certs/privkey.pem"
+#ADD certs/privkey.pem /etc/nginx/certs/
+echo "ADD nginx.conf /etc/nginx/conf.d/"
+echo "ADD nginx.conf.template /nginx.conf.template"
+
 echo "ENV ENVIRONMENT=$C_ENVIRONMENT"
 
 echo "CMD [\"/bin/sh\", \"-c\", \"envsubst '\$ENVIRONMENT' < /nginx.conf.template > /etc/nginx/conf.d/nginx.conf && exec nginx -g 'daemon off;'\"]"
