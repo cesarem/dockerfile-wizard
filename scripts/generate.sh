3aefe6b9013142a135e0a6e8e6cef9d4ec561c7b
Cesar Amaya <cesar.amaya@gmail.com>
2021-02-14 11:06:22 -0600

    privkey file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,8 +7,7 @@ echo "FROM nginxdemos/hello:plain-text"
 echo "RUN apk update && apk add openssl && mkdir /etc/nginx/certs"
 
 echo "ADD cert/fullchain.pem /etc/nginx/certs/"
-echo "ADD $PRIVKEY > /etc/nginx/certs/privkey.pem"
-#ADD certs/privkey.pem /etc/nginx/certs/
+echo "ADD privkey.pem /etc/nginx/certs/"
 echo "ADD nginx.conf /etc/nginx/conf.d/"
 echo "ADD nginx.conf.template /nginx.conf.template"
 
@@ -16,82 +15,3 @@ echo "ENV ENVIRONMENT=$C_ENVIRONMENT"
 
 echo "CMD [\"/bin/sh\", \"-c\", \"envsubst '\$ENVIRONMENT' < /nginx.conf.template > /etc/nginx/conf.d/nginx.conf && exec nginx -g 'daemon off;'\"]"
 
-if [ ! -e $RUBY_VERSION_NUM ] ; then
-    echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
-    tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
-    cd ruby-$RUBY_VERSION_NUM/ && \
-    ./configure && \
-    make -j4 && \
-    make install && \
-    ruby -v"
-fi
-
-if [ ! -e $NODE_VERSION_NUM ] ; then
-    echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION_NUM/node-v$NODE_VERSION_NUM.tar.gz && \
-    tar -xzvf node-v$NODE_VERSION_NUM.tar.gz && \
-    rm node-v$NODE_VERSION_NUM.tar.gz && \
-    cd node-v$NODE_VERSION_NUM && \
-    ./configure && \
-    make -j4 && \
-    make install && \
-    cd .. && \
-    rm -r node-v$NODE_VERSION_NUM"
-fi
-
-if [ ! -e $PYTHON_VERSION_NUM ] ; then
-    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
-    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
-    rm Python-$PYTHON_VERSION_NUM.tgz && \
-    cd Python-$PYTHON_VERSION_NUM && \
-    ./configure && \
-    make install"
-fi
-
-# if [ ! -e $PHP_VERSION_NUM ] ; then
-#     wget "http://php.net/distributions/php-${PHP_VERSION_NUM}.tar.xz"
-# fi
-
-if [ $JAVA = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; fi
-EOF
-fi
-
-if [ $MYSQL_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install mysql-client"
-fi
-
-if [ $POSTGRES_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install postgresql-client"
-fi
-
-if [ $DOCKERIZE = "true" ] ; then
-DOCKERIZE_VERSION="v0.6.1"
-
-cat << EOF
-RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-EOF
-fi
